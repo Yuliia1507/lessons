9915c415a9d00adf18ed3491b6c35f170cb75b07
Yuliia1507 <144613150+Yuliia1507@users.noreply.github.com>
2024-01-14 08:47:15 +0200

    Add files via upload

--- lesson_22/js/script.js
+++ lesson_22/js/script.js
@@ -3,18 +3,3 @@ const icon = document.querySelector('.icon-menu');
 icon.addEventListener('click', function () {
 	document.documentElement.classList.toggle('menu-open');
 });
-
-const header1 = document.querySelector('.first-header');
-const header2 = document.querySelector('.second-header');
-
-window.addEventListener('scroll', () => {
-	if (window.scrollY > header1.offsetHeight) {
-		header1.style.opacity = '0';
-		header2.style.position = 'fixed';
-		header2.style.top = '0';
-	} else {
-		header1.style.opacity = '1';
-		header2.style.position = 'absolute';
-		header2.style.top = header1.offsetHeight + 'px';
-	}
-});
